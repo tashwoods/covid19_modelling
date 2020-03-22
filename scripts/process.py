f84db3bcce11ca96215fde076a2b9cffb7e9de9e
Natasha Woods <natashawoods@Natashas-MacBook-Pro.local>
2020-03-22 17:36:39 -0500

    not stoked about these predictions at all

--- scripts/process.py
+++ scripts/process.py
@@ -1,4 +1,15 @@
 from imported_libraries import *
+
+def get_first_cv_day(country):
+  var_name = args.total_cases_var_name
+  first_cv_day = country[country[args.total_cases_var_name] > args.cv_day_thres].iloc[0].name
+  first_index = country.index.get_loc(first_cv_day)
+  #df.index.get_loc(result.iloc[0].name)
+  print(first_cv_day)
+  print(first_index)
+  return(first_index)
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description = 'arg parser for process.py')
   parser.add_argument('-f', '--input_file', type = str, dest = 'input_file', default = '../data/full_data.csv',  help = 'text file with input file names of countries')
@@ -8,6 +19,8 @@ if __name__ == '__main__':
   parser.add_argument('-plot_time_series', '--plot_time_series', type = int, dest = 'plot_time_series', default = 1, help = 'set to one to plot time series, zero to not')
   parser.add_argument('-date_name', '--date_name', type = str, dest = 'date_name', default = 'date', help = 'name of date columns in input file')
   parser.add_argument('-time_series_variables', '--time_series_variables', type = list, dest = 'time_series_variables', default = ['new_cases', 'new_deaths', 'total_cases', 'total_deaths'], help = 'list of variables to plot in time series')
+  parser.add_argument('-total_cases_var_name', '--total_cases_var_name', type = str, dest = 'total_cases_var_name', default = 'total_cases', help = 'name of total cases variable name in input file')
+  parser.add_argument('-cv_day_thres', '--cv_day_thres', type = int, dest = 'cv_day_thres', default = 3, help = 'total number of cases to consider it the first day of cv19')
   args = parser.parse_args()
 
 
@@ -46,6 +59,19 @@ if __name__ == '__main__':
       plt.savefig(var + '_normalized_overlay.pdf')
       plt.close('all')
 
+      #Plot normalized plots with dates aligned
+      for country in country_dataframes_list:
+        start_date = get_first_cv_day(country)
+        country = country[start_date:]
+        country_name = country[args.country_var_name].iloc[0]
+        population = CountryInfo(country_name).population()
+        plt.plot(country[args.date_name], country[var]/population, label = country[args.country_var_name].iloc[0])
+      plt.title('Normalized Date-Shifted' + var + ' vs. ' + args.date_name)
+      plt.legend()
+      plt.savefig(var + '_normalized_date_shifted_overlay.pdf')
+      plt.close('all')
+      
+    print('done')
 
 
 
