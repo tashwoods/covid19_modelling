eb2835206a8b24410a4daea4e08b8a7d5eaeef4d
Natasha Woods <natashawoods@Natashas-MacBook-Pro.local>
2020-03-23 14:26:37 -0500

    adding state level data and fixed cv day type plots for country level

--- scripts/process.py
+++ scripts/process.py
@@ -4,15 +4,12 @@ def get_first_cv_day(country):
   var_name = args.total_cases_var_name
   first_cv_day = country[country[args.total_cases_var_name] > args.cv_day_thres].iloc[0].name
   first_index = country.index.get_loc(first_cv_day)
-  #df.index.get_loc(result.iloc[0].name)
-  print(first_cv_day)
-  print(first_index)
   return(first_index)
-
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description = 'arg parser for process.py')
   parser.add_argument('-f', '--input_file', type = str, dest = 'input_file', default = '../data/full_data.csv',  help = 'text file with input file names of countries')
+  parser.add_argument('-state_file', '--state_file', type = str, dest = 'state_file', default = '../data/us_states_covid19_daily.csv', help = 'file with us state level data')
   parser.add_argument('-c', '--countries_file', type = str, dest = 'countries_file', default = 'countries.txt', help = 'Name of text file with names of countries to process')
   parser.add_argument('-o', '--output_dir', type = str, dest = 'output_dir', default = 'output', help = 'name of output_dir')
   parser.add_argument('-country_var_name', '--country_var_name', type = str, dest = 'country_var_name', default = 'location', help = 'country variable name in input dataset')
@@ -21,14 +18,18 @@ if __name__ == '__main__':
   parser.add_argument('-time_series_variables', '--time_series_variables', type = list, dest = 'time_series_variables', default = ['new_cases', 'new_deaths', 'total_cases', 'total_deaths'], help = 'list of variables to plot in time series')
   parser.add_argument('-total_cases_var_name', '--total_cases_var_name', type = str, dest = 'total_cases_var_name', default = 'total_cases', help = 'name of total cases variable name in input file')
   parser.add_argument('-cv_day_thres', '--cv_day_thres', type = int, dest = 'cv_day_thres', default = 3, help = 'total number of cases to consider it the first day of cv19')
+  parser.add_argument('-tick_font_size', '--tick_font_size', type = int, dest = 'tick_font_size', default = 8, help = 'size of tick labels in plots')
   args = parser.parse_args()
-
 
   #Make output data directory and get input data
   make_output_dir(args.output_dir)
   full_dataframe = pd.read_csv(args.input_file, parse_dates = [args.date_name])
-  print('changed')
 
+
+  state_dataframe = pd.read_csv(args.state_file, parse_dates = [args.date_name])
+  state_dataframe.fillna(0, inplace=True)
+  print(full_dataframe)
+  print(state_dataframe)
   #Process make country dataframe and objects
   country_dataframes_list = list()
   countries_file = open(args.countries_file, "r")
@@ -44,34 +45,45 @@ if __name__ == '__main__':
       for country in country_dataframes_list:
         plt.plot(country[args.date_name], country[var], label = country[args.country_var_name].iloc[0])
       plt.title(var + ' vs. ' + args.date_name)
+      plt.xlabel(args.date_name)
+      plt.ylabel(var)
       plt.legend()
-      plt.savefig(var + '_unmodified_overlay.pdf')
+      plt.xticks(fontsize = args.tick_font_size)
+      plt.savefig(args.output_dir + '/' + var + '_unmodified_overlay.pdf')
       plt.close('all')
-    
 
       #Plot normalized plots without creating same start date
       for country in country_dataframes_list:  
         country_name = country[args.country_var_name].iloc[0]
         population = CountryInfo(country_name).population()
-        plt.plot(country[args.date_name], country[var]/population, label = country[args.country_var_name].iloc[0])
+        plt.plot(country[args.date_name], 100*(country[var]/population), label = country[args.country_var_name].iloc[0])
       plt.title('Normalized ' + var + ' vs. ' + args.date_name)
+      plt.xlabel(args.date_name)
+      plt.ylabel('Percent of country population: ' + var)
       plt.legend()
-      plt.savefig(var + '_normalized_overlay.pdf')
+      plt.xticks(fontsize = args.tick_font_size)
+      plt.savefig(args.output_dir + '/' + var + '_normalized_overlay.pdf')
       plt.close('all')
 
       #Plot normalized plots with dates aligned
       for country in country_dataframes_list:
         start_date = get_first_cv_day(country)
-        country = country[start_date:]
+        country = country[start_date:].reset_index()
         country_name = country[args.country_var_name].iloc[0]
         population = CountryInfo(country_name).population()
-        plt.plot(country[args.date_name], country[var]/population, label = country[args.country_var_name].iloc[0])
-      plt.title('Normalized Date-Shifted' + var + ' vs. ' + args.date_name)
+        print(country_name) 
+        print(population)
+        print(country.index.values)
+        plt.plot(country.index.values, 100*(country[var]/population), label = country[args.country_var_name].iloc[0])
+      plt.title('Normalized Date-Shifted ' + var + ' vs. ' + args.date_name)
+      plt.xlabel('COVID-19 Outbreak Days')
+      plt.ylabel('Percent of country population: ' + var)
+
       plt.legend()
-      plt.savefig(var + '_normalized_date_shifted_overlay.pdf')
+      plt.savefig(args.output_dir + '/' + var + '_normalized_date_shifted_overlay.pdf')
       plt.close('all')
       
-    print('done')
 
+  
 
 
